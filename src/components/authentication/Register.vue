a575f74787ed6471cc5eee6d586b7c4bf45668d4
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-16 21:51:59 +0700

    component->login,register

---
+++ src/components/authentication/Register.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="form-group">
+						<input type="text" class="form-control" v-model="name" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<input type="email" class="form-control" v-model="email" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control" v-model="password" placeholder="Password">
+					</div>
+					<button class="btn btn-success pull-right"> Register</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name: '',
+				email: '',
+				password: ''
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
