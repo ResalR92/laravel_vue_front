22fe3b1efec0b107ed40106f8ffbc935e7bbbce9
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-17 11:33:06 +0700

    package->auth.js->object->vue->main.js->register global->this.$auth.setToken(token,expires_in)->localStorage.setItem(token,expires_in)

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import Router from './routes.js'
 
 import VueResource from 'vue-resource'
+//register packages globally
+import Auth from './packages/auth/Auth.js'
 
 Vue.use(VueResource)
+Vue.use(Auth)
 
 new Vue({
   el: '#app',
---
+++ src/packages/auth/Auth.js
@@ -0,0 +1,30 @@
+export default function(Vue) {
+	Vue.auth = {
+		//set token
+		setToken: (token, expiration) => {
+			localStorage.setItem('token',token);
+			localStorage.setItem('expiration',expiration);
+		},
+		//get token
+		getToken: () => {
+
+		},
+		//destroy token
+		//isAuthenticated
+		isAuthenticated: () => {
+			if(this.getToken()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	Object.defineProperties(Vue.prototype, {
+		$auth: {
+			get: () => {
+				return Vue.auth
+			}
+		}
+	})
+}
